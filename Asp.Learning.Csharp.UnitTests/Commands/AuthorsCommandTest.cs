04feac9768cfbc8c5128b14a273be15bd1d97078
Oliver.Gildaro <ocastro@fmsystems.com>
2025-03-28 13:17:33 -0400

    aasas

---
+++ Asp.Learning.Csharp.UnitTests/Commands/AuthorsCommandTest.cs
@@ -0,0 +1,57 @@
+﻿using Asp.Learning.Commanding.Commands;
+using Asp.Learning.Commanding.Commands.AddBookToAuthor;
+using Asp.Learning.Commanding.Commands.CreateAuthor;
+using Asp.Learning.Contracts;
+using Asp.Learning.repositories.Entities;
+using Moq;
+
+namespace Asp.Learning.Csharp.UnitTests.Commands
+{
+    //private ICommandHandler<AddBookToAuthorCommand, Guid> command;
+    public class AuthorsCommandTest
+    {
+        private CreateAuthorCommandHandler commandHandler;
+        public AuthorsCommandTest()
+        {
+            //***** CONSTRUCTOR CONTEXT *****//////
+            var repo = new Mock<IWriteRepository<Author>>();
+            repo.Setup(x => x.AddAsync(It.IsAny<Author>()))
+                .ReturnsAsync(Guid.NewGuid);
+
+            commandHandler = new CreateAuthorCommandHandler(repo.Object);
+        }
+
+        [Theory]
+        [InlineData(
+    "Oliver",
+    "Castro",
+    "1986-07-23T00:00:00+00:00",
+    "1986-07-24T00:00:00+00:00",
+    "Poetry")]
+        [InlineData(
+    "Alvaro",
+    "Castro",
+    "1986-07-23T00:00:00+00:00",
+    "1986-07-24T00:00:00+00:00",
+    "Poetry")]
+        public async Task Test_CreateAuthorCommand(string firstName,
+    string lastName,
+    string DateOfBirth,
+    string DateOfDeath,
+    string MainCategory)
+        {
+            var author = new CreateAuthorCommand
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                DateOfBirth = DateTime.Parse(DateOfBirth),
+                DateOfDeath = DateTime.Parse(DateOfDeath),
+                MainCategory = MainCategory
+            };
+
+            var result = await this.commandHandler.HandleAsync(author);
+
+            Assert.IsType<Guid>(result);
+        }
+    }
+}
---
+++ Asp.Learning.Csharp.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Asp.Learning.Csharp.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
